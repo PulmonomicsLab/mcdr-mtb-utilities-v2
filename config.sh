48dc5af1a484b4d86c2e351c676cd7f274c2a294
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2024-05-09 20:10:37 +0530

    Store configuration parameters

---
+++ config.sh
@@ -0,0 +1,9 @@
+freebayes_path=/usr/bin/freebayes
+samtools_path=/usr/bin/samtools
+bwa_path=/usr/bin/bwa
+trim_galore_path=/usr/bin/trim_galore
+bgzip_path=/usr/bin/bgzip
+bcftools_path=/usr/bin/bcftools
+trim_galore_cores=4
+bwa_mem_cores=4
+samtools_cores=4
